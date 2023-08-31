ec9c45c556a27f1a2bbe80ec71d83a84d614f000
Pasha Mango <141678664+pashamango@users.noreply.github.com>
2023-08-31 16:28:14 -0700

    Create init_repo.sh

---
+++ init_repo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Initialize GitHub repository for ChitAkasha
+
+# Create initial directories and files
+mkdir QuantumLibraries
+mkdir QuantumExtensions
+touch README.md
+touch QuantumScript.qs
