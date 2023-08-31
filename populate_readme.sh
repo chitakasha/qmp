f469778b9ad9f6d992e993fe15d66e7ca4d5d8a7
Pasha Mango <141678664+pashamango@users.noreply.github.com>
2023-08-31 16:28:46 -0700

    Create populate_readme.sh

---
+++ populate_readme.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Populate README.md
+
+echo "# ChitAkasha Quantum-Genomic Ecosystem" > README.md
+echo "## Introduction" >> README.md
+echo "This repository serves as the blueprint for the ChitAkasha ecosystem..." >> README.md
